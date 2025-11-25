a912363c8280d20ab4c5a85465291629f27541da
Lakeshsaikumar <keerthilakesh77@gmail.com>
2025-11-25 12:53:15 +0530

    Update RestControllerAspect.java

--- src/main/java/com/khoubyari/example/RestControllerAspect.java
+++ src/main/java/com/khoubyari/example/RestControllerAspect.java
@@ -1,8 +1,6 @@
 package com.khoubyari.example;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -14,10 +12,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class RestControllerAspect {
 
-    protected final Logger log = LoggerFactory.getLogger(this.getClass());
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    @Before("execution(public * com.khoubyari.example.api.rest.*Controller.*(..))")
-    public void logBeforeRestCall(JoinPoint pjp) throws Throwable {
-        log.info(":::::AOP Before REST call:::::" + pjp);
+    // Named pointcut: target only controller methods annotated with @GetMapping (or other mappings)
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+    public void getMappingMethods() {}
+
+    // Alternatively, if you want *all* controller methods, but within a specific package:
+    // @Pointcut("within(com.khoubyari.example.api.rest..*) && execution(public * *(..))")
+    // public void allRestControllerMethods() {}
+
+    @Before("getMappingMethods()")
+    public void logBeforeRestCall(JoinPoint pjp) {
+        // Log more context to help debugging
+        log.info("AOP Before REST call → method: {}, signature: {}, thread: {}",
+                pjp.getSignature().toShortString(),
+                pjp.getSignature().toLongString(),
+                Thread.currentThread().getName());
+        log.debug("Target class: {}", pjp.getTarget().getClass().getName());
     }
 }
